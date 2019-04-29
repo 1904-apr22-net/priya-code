78d05543313a8491a5272cf08e5bd88b57a50893
msunnypriyanka@gmail.com <msunnypriyanka@gmail.com>
2019-04-29 11:50:22 -0500

    palin prgm

---
+++ assessment-weak1/Program/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            string s, revs = "";
+            Console.WriteLine("Enter string");
+            s= Console.ReadLine();
+            for(int i = s.Length - 1; i >= 0; i--)
+            {
+                revs = revs + s[i].ToString();
+            }
+            if (revs == s)
+            {
+                Console.WriteLine("Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("not Palindrome");
+
+            }
+        }
+    }
+}
